91f9dbce750276b37b9570e49aba11b3f3b6e256
Lisa Stenson <lstenson18@yahoo.com>
2017-02-16 12:34:32 -0800

    lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -25,8 +25,23 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
+		$.get("/project/" + idNumber, function(result) {console.log(result); 
+		var projectHTML = '<a href="#" class="thumbnail">' + '<img src="' + result['image'] + 
+		'" class="detailsImage">' + '<p>' + result['title'] + '</p>' + '<p><small>' 
+		+ result['date'] + '</small></p/a>' + result['summary']; 
+		console.log("this shit sucks " + projectHTML)
+		$(".details", idNumber);
+		console.log('this is our id #' + idNumber);
+		$("div #project"+ idNumber + " .details").html(projectHTML);
+		
+	});
 
 	console.log("User clicked on project " + idNumber);
+}
+
+
+function callbackFN(result){
+
 }
 
 /*
@@ -35,4 +50,21 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$('.jumbotron h1').text("Javascript is connected");
+	$("#testjs").text("Please wait...");
+	$(".jumbotron p").toggleClass("active");
+	$.get("/palette", randomColor);
 }
+
+
+function randomColor(result) {
+	var hex = result['colors'];
+	var colors = hex['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
+
